13f36f45264ca058276f87da17b8b07609083552
Jonas Holbech <jh@jonasholbech.dk>
2022-12-30 14:35:31 +0100

    add select

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,51 @@
 <script>
-  import {onMount} from "svelte";
-  let lyrics=[];
+  import { onMount } from "svelte";
+  let lyrics = [];
   let time = 0;
-	let duration;
-	let paused = true;
-  onMount(async ()=>{
-    const response = await fetch("lyrics/kryptonite.txt");
-    const data = await response.text();
-    const splitted = data.split("\n")
-    const x = splitted.map(line=>{
-      const [time, lyric] = line.split("]")
+  let track = "kryptonite";
+  let player;
+  onMount(getData);
+  async function getData() {
+    const data = await fetch(`lyrics/${track}.txt`).then((res) => res.text());
+    player.src = `audio/${track}.mp3`;
+
+    lyrics = data.split("\n").map((line) => {
+      const [time, lyric] = line.split("]");
       return {
-        time:getTimeInSeconds(time),
-        lyric
-      }
-    })
-    lyrics = x;
-  })
-  $: timeInMS = Math.floor(time*1000);
-  function getTimeInSeconds(str){
-    let cleaned = str.replace("[","");
-    let [minutes,rest] = cleaned.split(":")
-    let [seconds, milliseconds] = rest.split(".")
-    let total = Number(minutes) * 60 * 1000 + Number(seconds) * 1000 + Number(milliseconds) * 10
-    return total;
+        time: getTimeInSeconds(time),
+        lyric,
+      };
+    });
   }
-  //[00:20.05]
+  function getTimeInSeconds(str) {
+    let [minutes, seconds, milliseconds] = str.replace("[", "").split(/:|\./);
+    return minutes * 60 + +seconds + milliseconds / 100;
+  }
 </script>
 
 <main>
- <audio controls 
-    bind:currentTime={time}
-		bind:duration
-		bind:paused>
-  <source src="audio/kryptonite.mp3" type="audio/mpeg"></audio> 
-<!-- <p>{timeInMS}</p> -->
- {#each lyrics as lyric (lyric.time)}
-    <p data-time={lyric.time} class:current={timeInMS>lyric.time}>{lyric.lyric}</p>
- {/each}
+  <div>
+    <select bind:value={track} on:change={getData}>
+      <option value="kryptonite">Kryptonite</option>
+      <option value="killingit">Killing It</option>
+    </select>
+  </div>
+
+  <audio controls bind:currentTime={time} preload="auto" bind:this={player} />
+
+  {#each lyrics as lyric (lyric.time)}
+    <p data-time={lyric.time} class:current={time > lyric.time}>
+      {lyric.lyric}
+    </p>
+  {/each}
 </main>
 
-<style global>
+<style>
   .current {
-    color:hotpink;
+    color: hotpink;
+    font-weight: normal;
   }
-  
-  .current {
-    font-weight:bold;
+  .current:not(:has(~ .current)) {
+    font-weight: bold;
   }
-  /* .current + .current {
-    font-weight:normal;
-  } */
-    
-  /* .current:has( + :not(.current)) {
-      font-weight:bold;
-    } */
-  
 </style>
